68cc43efbab085749f566033a5a7137bc691d84a
YannickMyxe <vankerckvoordeyannick@gmail.com>
2021-04-25 17:07:00 +0200

    Added item to the registry and added a texture from suited lama

--- src/main/java/myxej/yannick/mud/Mud.java
+++ src/main/java/myxej/yannick/mud/Mud.java
@@ -3,15 +3,20 @@ package myxej.yannick.mud;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Mud implements ModInitializer {
 
-	public static final Item mud_ball = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final Item MUD_BALL = new Item(new Item.Settings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
 		// Leaving this as a debug message
 		System.out.println("Hello Fabric world!");
 		System.out.println("Mud mod setting up");
+
+		Registry.register(Registry.ITEM, new Identifier("mud", "mud_ball"), MUD_BALL);
+
 	}
 }
